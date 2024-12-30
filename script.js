3b628be5ba5b3f7ea6a20e14e0f026bfee8f91fe
Lestari Widodo <lwidodo.ridho@gmail.com>
2024-12-31 01:05:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -148,3 +148,21 @@ function updateClock() {
 
 setInterval(updateClock, 1000);
 updateClock(); // Initial call to set the clock immediately
+
+// Togle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika hamburger Menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//Klik diluar side bar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+// Membuat function untuk menghilangkan hamburger-menu apabila mouse diklik diluar navbar dan hamburger-menu.
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
